ef201607e830efaae507d8859ab8a50c97a3e007
Peitian <pp482@cornell.edu>
2018-08-30 19:15:41 -0400

    Optimize the RTL model of GCD unit. (1) The calculation is done whenever a or b reaches zero (2) Streaming data input is enabled by modifying the control FSM (3) Swap and subtraction can now be executed at the same time

--- sim/tut3_pymtl/gcd/GcdUnitRTL.py
+++ sim/tut3_pymtl/gcd/GcdUnitRTL.py
@@ -13,16 +13,27 @@ from GcdUnitMsg  import GcdUnitReqMsg
 # Constants
 #=========================================================================
 
-A_MUX_SEL_NBITS = 2
+# *_MUX_SEL_X: a dont-care case. Make sure the output of MUX is ignored!
+
+A_MUX_SEL_NBITS = 1
 A_MUX_SEL_IN    = 0
 A_MUX_SEL_SUB   = 1
-A_MUX_SEL_B     = 2
 A_MUX_SEL_X     = 0
 
 B_MUX_SEL_NBITS = 1
-B_MUX_SEL_A     = 0
-B_MUX_SEL_IN    = 1
+B_MUX_SEL_IN    = 0
+B_MUX_SEL_SUB   = 1
 B_MUX_SEL_X     = 0
+
+O_MUX_SEL_NBITS = 1
+O_MUX_SEL_A     = 0
+O_MUX_SEL_B     = 1
+O_MUX_SEL_X     = 0
+
+S_MUX_SEL_NBITS = 1
+S_MUX_SEL_AB    = 0
+S_MUX_SEL_BA    = 1
+S_MUX_SEL_X     = 0
 
 #=========================================================================
 # GCD Unit RTL Datapath
@@ -48,9 +59,12 @@ class GcdUnitDpathRTL (Model):
     s.a_reg_en  = InPort  (1)
     s.b_mux_sel = InPort  (B_MUX_SEL_NBITS)
     s.b_reg_en  = InPort  (1)
+    s.o_mux_sel = InPort  (O_MUX_SEL_NBITS)
+    s.s_mux_sel = InPort  (S_MUX_SEL_NBITS)
 
     # Status signals (dpath -> ctrl)
 
+    s.is_a_zero = OutPort (1)
     s.is_b_zero = OutPort (1)
     s.is_a_lt_b = OutPort (1)
 
@@ -63,12 +77,11 @@ class GcdUnitDpathRTL (Model):
     s.sub_out   = Wire(16)
     s.b_reg_out = Wire(16)
 
-    s.a_mux = m = Mux( 16, 3 )
+    s.a_mux = m = Mux( 16, 2 )
     s.connect_pairs(
       m.sel,                  s.a_mux_sel,
       m.in_[ A_MUX_SEL_IN  ], s.req_msg_a,
       m.in_[ A_MUX_SEL_SUB ], s.sub_out,
-      m.in_[ A_MUX_SEL_B   ], s.b_reg_out,
     )
 
     # A register
@@ -83,9 +96,9 @@ class GcdUnitDpathRTL (Model):
 
     s.b_mux = m = Mux( 16, 2 )
     s.connect_pairs(
-      m.sel,                 s.b_mux_sel,
-      m.in_[ B_MUX_SEL_A  ], s.a_reg.out,
-      m.in_[ B_MUX_SEL_IN ], s.req_msg_b,
+      m.sel,                  s.b_mux_sel,
+      m.in_[ B_MUX_SEL_IN  ], s.req_msg_b,
+      m.in_[ B_MUX_SEL_SUB ], s.sub_out,
     )
 
     # B register
@@ -97,7 +110,42 @@ class GcdUnitDpathRTL (Model):
       m.out, s.b_reg_out,
     )
 
-    # Zero compare
+    # O mux
+
+    s.o_mux = m = Mux( 16, 2 )
+    s.connect_pairs(
+      m.sel,                 s.o_mux_sel,
+      m.in_[ O_MUX_SEL_A  ], s.a_reg.out,
+      m.in_[ O_MUX_SEL_B  ], s.b_reg.out,
+    )
+
+    # S mux (minuend)
+
+    s.minuend_mux = m = Mux( 16, 2 )
+    s.connect_pairs(
+      m.sel,                  s.s_mux_sel,
+      m.in_[ S_MUX_SEL_AB  ], s.a_reg.out,
+      m.in_[ S_MUX_SEL_BA  ], s.b_reg.out,
+    )
+
+    # S mux (subtrahend)
+
+    s.subtrahend_mux = m = Mux( 16, 2 )
+    s.connect_pairs(
+      m.sel,                  s.s_mux_sel,
+      m.in_[ S_MUX_SEL_AB  ], s.b_reg.out,
+      m.in_[ S_MUX_SEL_BA  ], s.a_reg.out,
+    )
+
+    # A zero compare
+
+    s.a_zero = m = ZeroComparator(16)
+    s.connect_pairs(
+      m.in_, s.a_reg.out,
+      m.out, s.is_a_zero,
+    )
+
+    # B zero compare
 
     s.b_zero = m = ZeroComparator(16)
     s.connect_pairs(
@@ -118,14 +166,14 @@ class GcdUnitDpathRTL (Model):
 
     s.sub = m = Subtractor(16)
     s.connect_pairs(
-      m.in0, s.a_reg.out,
-      m.in1, s.b_reg.out,
+      m.in0, s.minuend_mux.out, 
+      m.in1, s.subtrahend_mux.out,
       m.out, s.sub_out,
     )
 
     # connect to output port
 
-    s.connect( s.sub.out, s.resp_msg )
+    s.connect( s.o_mux.out, s.resp_msg )
 
 #=========================================================================
 # GCD Unit RTL Control
@@ -149,12 +197,17 @@ class GcdUnitCtrlRTL (Model):
 
     s.a_mux_sel = OutPort (A_MUX_SEL_NBITS)
     s.a_reg_en  = OutPort (1)
-    s.b_mux_sel = OutPort (1)
+    s.b_mux_sel = OutPort (B_MUX_SEL_NBITS)
     s.b_reg_en  = OutPort (1)
+    # select the output: is it reg_a or reg_b?
+    s.o_mux_sel = OutPort (O_MUX_SEL_NBITS)
+    # select minuend and subtrahend
+    s.s_mux_sel = OutPort (S_MUX_SEL_NBITS)
 
     # Status signals (dpath -> ctrl)
 
-    s.is_b_zero = InPort  (B_MUX_SEL_NBITS)
+    s.is_a_zero = InPort  (1)
+    s.is_b_zero = InPort  (1)
     s.is_a_lt_b = InPort  (1)
 
     # State element
@@ -168,6 +221,13 @@ class GcdUnitCtrlRTL (Model):
     #---------------------------------------------------------------------
     # State Transition Logic
     #---------------------------------------------------------------------
+
+    # flag indicating whether the computation is done
+    s.is_finished   = Wire(1)
+
+    @s.combinational
+    def assign_is_finished():
+      s.is_finished.value = s.is_a_zero or s.is_b_zero
 
     @s.combinational
     def state_transitions():
@@ -184,7 +244,9 @@ class GcdUnitCtrlRTL (Model):
       # Transistions out of CALC state
 
       if ( curr_state == s.STATE_CALC ):
-        if ( not s.is_a_lt_b and s.is_b_zero ):
+        if ( s.is_finished and s.resp_rdy and not s.req_val ): 
+          next_state = s.STATE_IDLE
+        elif ( s.is_finished and not ( s.resp_rdy and s.req_val ) ):
           next_state = s.STATE_DONE
 
       # Transistions out of DONE state
@@ -199,8 +261,49 @@ class GcdUnitCtrlRTL (Model):
     # State Output Logic
     #---------------------------------------------------------------------
 
-    s.do_swap = Wire(1)
-    s.do_sub  = Wire(1)
+    s.do_swap   =   Wire(1)
+    s.do_sub    =   Wire(1)
+
+    # compute sel of A_MUX
+    s.a_mux_sel_value   =   Wire(A_MUX_SEL_NBITS)
+
+    @s.combinational
+    def a_mux_set_val_gen():
+      if ( ~s.is_finished ):
+        # do_swap
+        if ( s.is_a_lt_b ): 
+          s.a_mux_sel_value.value = A_MUX_SEL_X
+        # do_sub
+        else: 
+          s.a_mux_sel_value.value = A_MUX_SEL_SUB
+      else:
+        # be ready to accept next input
+        if ( s.resp_rdy ): 
+          s.a_mux_sel_value.value = A_MUX_SEL_IN
+        # req_rdy will be 0, so dont care 
+        else:
+          s.a_mux_sel_value.value = A_MUX_SEL_X
+
+    # compute sel of B_MUX
+    s.b_mux_sel_value   =   Wire(B_MUX_SEL_NBITS)
+
+    @s.combinational
+    def b_mux_set_val_gen():
+      if ( ~s.is_finished ):
+        # do_swap
+        if ( s.is_a_lt_b ): 
+          s.b_mux_sel_value.value = B_MUX_SEL_SUB
+        # do_sub
+        else: 
+          # dont-care since reg_b will be disabled 
+          s.b_mux_sel_value.value = B_MUX_SEL_X
+      else:
+        # be ready to accept next input
+        if ( s.resp_rdy ): 
+          s.b_mux_sel_value.value = B_MUX_SEL_IN
+        # req_rdy will be 0, so dont care 
+        else:
+          s.b_mux_sel_value.value = B_MUX_SEL_X
 
     @s.combinational
     def state_outputs():
@@ -216,20 +319,34 @@ class GcdUnitCtrlRTL (Model):
         s.a_reg_en.value  = 1
         s.b_mux_sel.value = B_MUX_SEL_IN
         s.b_reg_en.value  = 1
+        # s.resp_val = 0 so the output of O_MUX will be ignored
+        s.o_mux_sel.value = O_MUX_SEL_X
+        # a.en = 0 and b.en = 0 so the output of S_MUX will be ignored
+        s.s_mux_sel.value = S_MUX_SEL_X
 
       # In CALC state we iteratively swap/sub to calculate GCD
 
       elif current_state == s.STATE_CALC:
 
-        s.do_swap.value = s.is_a_lt_b
-        s.do_sub.value  = ~s.is_b_zero
+        s.do_swap.value   = s.is_a_lt_b
+        s.do_sub.value    = ~s.is_b_zero
 
-        s.req_rdy.value   = 0
-        s.resp_val.value  = 0
-        s.a_mux_sel.value = A_MUX_SEL_B if s.do_swap else A_MUX_SEL_SUB
-        s.a_reg_en.value  = 1
-        s.b_mux_sel.value = B_MUX_SEL_A
-        s.b_reg_en.value  = s.do_swap
+        s.req_rdy.value   = s.is_finished and s.resp_rdy
+        s.resp_val.value  = s.is_finished
+
+        # see combinational block for cases where we need enable signal
+        s.a_mux_sel.value = s.a_mux_sel_value
+        s.a_reg_en.value  = ( ~s.is_finished and ~s.do_swap ) or \
+                            ( s.is_finished and s.resp_rdy )
+
+        s.b_mux_sel.value = s.b_mux_sel_value
+        s.b_reg_en.value  = ( ~s.is_finished and s.do_swap ) or \
+                            ( s.is_finished and s.resp_rdy )
+
+        # choose whichever is not zero as output
+        s.o_mux_sel.value = O_MUX_SEL_A if s.is_b_zero else O_MUX_SEL_B
+        # if a < b then perform b - a
+        s.s_mux_sel.value = S_MUX_SEL_BA if s.do_swap else S_MUX_SEL_AB
 
       # In DONE state we simply wait for output transaction to occur
 
@@ -240,6 +357,10 @@ class GcdUnitCtrlRTL (Model):
         s.a_reg_en.value  = 0
         s.b_mux_sel.value = B_MUX_SEL_X
         s.b_reg_en.value  = 0
+        # choose whichever is not zero as output
+        s.o_mux_sel.value = O_MUX_SEL_A if s.is_b_zero else O_MUX_SEL_B
+        # a.en = 0 and b.en = 0 so the output of S_MUX will be ignored
+        s.s_mux_sel.value = S_MUX_SEL_X
 
 #=========================================================================
 # GCD Unit RTL Model
